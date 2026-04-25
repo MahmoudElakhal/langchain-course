4936e04ea13528ee1ec7473277447ffc268065f2
Mahmoud Elakhal <130182158+MahmoudElakhal@users.noreply.github.com>
2026-04-25 16:25:15 +0300

    search agent using langchain tavily built in module

--- 2-react-serach-agent/main.py
+++ 2-react-serach-agent/main.py
@@ -3,30 +3,14 @@ from langchain_core.messages import HumanMessage
 from langchain_openai import ChatOpenAI
 from langchain.tools import tool
 from langchain.agents import create_agent
-from tavily import TavilyClient
+from langchain_tavily import TavilySearch
+
 
 load_dotenv()
 
-tavily_client = TavilyClient()
-
-
-@tool
-def search(query : str) -> str:
-    """
-    Tool that dearches over the internet
-    Args: 
-        query : The query to search for
-    
-    Returns:
-        The search results
-    """
-    print(f"Searching for: {query}")
-    return tavily_client.search(query=query)
-
-
 
 llm = ChatOpenAI(model ="gpt-5")
-tools = [search]
+tools = [TavilySearch()]
 agent = create_agent(llm, tools)  
 
 
