dc7b946439accfb68fc2052062190d0e1097ead0
Mahmoud Elakhal <130182158+MahmoudElakhal@users.noreply.github.com>
2026-04-26 00:13:22 +0300

    Adding Structured output

--- 2-react-serach-agent/main.py
+++ 2-react-serach-agent/main.py
@@ -1,3 +1,6 @@
+from typing import List
+from pydantic import BaseModel  ,Field
+
 from dotenv import load_dotenv
 from langchain_core.messages import HumanMessage
 from langchain_openai import ChatOpenAI
@@ -5,13 +8,25 @@ from langchain.tools import tool
 from langchain.agents import create_agent
 from langchain_tavily import TavilySearch
 
-
 load_dotenv()
+
+# This Class is going to represent the source of the answer
+class Source(BaseModel):
+
+    """ Schema for a source used by the agent
+    """    
+    url:str = Field(description="The URL of the source")
+
+class AgentResponse(BaseModel):
+    """Scehma for agent response with answer and sources"""
+    answer:str = Field(description = "The Agent's answer to the query") 
+    sources:List[Source] = Field(default_factory = list , description = "List of Sources used to generate the answer")
+
 
 
 llm = ChatOpenAI(model ="gpt-5")
 tools = [TavilySearch()]
-agent = create_agent(llm, tools)  
+agent = create_agent(llm, tools , response_format = AgentResponse)  
 
 
 def main():
